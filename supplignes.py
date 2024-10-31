07ccb840d89aa0a29f698a42b5b7e805467daf0c
GIGLIONI-Maellys <159936244+GIGLIONI-Maellys@users.noreply.github.com>
2024-10-31 16:32:12 +0100

    Update supplignes.py

--- supplignes.py
+++ supplignes.py
@@ -1,6 +1,9 @@
 def supplignes (grid) : 
+  #fonction qui va supprimer les lignes remplies dans la grille et qui va renvoyer le nombre de lignes supprimées 
   nb_lignes_supp=0
+  #boucle while qui va parcourir les sous listes, soit les lignes de la grille
   while i<= len(grid[][]) :
+    #boucle for qui va parcourir les diverses éléments de la lignes et les passer à vide si il n'y a pas de zéro, c'est à dire qu'elle est remplie 
     for i in range(len(grid)) : 
       if 0 not in grid[i] : 
         for j in range(len(grid[i]) : 
@@ -9,6 +12,7 @@ def supplignes (grid) :
   return nb_lignes_supp ; 
 
 def points (compteur, nb_lignes_supp) : 
+  #fonction qui va ajouter les points selon le nombre de lignes supprimées 
   if nb_lignes_supp==1 : 
     compteur += 40
   elif nb_lignes_supp ==2 : 
@@ -18,3 +22,8 @@ def points (compteur, nb_lignes_supp) :
   elif nb_lignes_supp == 4 : 
     compteur += 500 
 return compteur ; 
+
+compteur = 0 
+
+
+  
