36a15a1760d6af7bf1b6db2807ba8eee40c0a6bd
GIGLIONI-Maellys <159936244+GIGLIONI-Maellys@users.noreply.github.com>
2024-10-31 16:21:54 +0100

    Update supplignes.py

--- supplignes.py
+++ supplignes.py
@@ -1,7 +1,21 @@
-def supplignes (grid, position_piece) : 
+def supplignes (grid) : 
+  nb_lignes_supp=0
   while i<= len(grid[][]) :
-    if grid[][] == 1 : 
-      grid[][] = 0
-    i+=1 
+    for i in range(len(grid)) : 
+      if 0 not in grid[i] : 
+        for j in range(len(grid[i]) : 
+          grid[i][j]=0
+        nb_lignes_supp+=1
+  return nb_lignes_supp ; 
 
-  return grid ; 
+def points (nb_lignes_supp) : 
+  compteur = 0 
+  if nb_lignes_supp==1 : 
+    compteur += 40
+  elif nb_lignes_supp ==2 : 
+    compteur += 100 
+  elif nb_lignes_supp == 3 : 
+    compteur += 300
+  elif nb_lignes_supp == 4 : 
+    compteur += 500 
+return compteur ; 
